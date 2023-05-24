28a0f19593c0c01774983e25c0be9a965f8ade6b
AbdullahAli1999 <iifamxx@gmail.com>
2023-05-24 19:20:53 +0300

    404 pages

--- alkenazksa/src/pages/NotFound.js
+++ alkenazksa/src/pages/NotFound.js
@@ -1,7 +1,12 @@
+import './pages_css/notfound.css';
+import Home from '../pages/Home.js';
+import p404p from '../imagess/404.png';
+import Header from "../componets/Header"
 export default function NotFound(){
     return(
-        <div>
-            <h1>Ooops! 404 Not found</h1>
+        <div class="aa">
+        <img src={p404p} class="four-0-four-image" alt=""/>
+        <p class="four-0-four-msg">look like you are lost. Head to beack to our <a href='/'>Alkenaz</a></p>
         </div>
     )
 }
