b688025cbd8b15e372c697a3a6047dcce985b057
AbdullahAli1999 <iifamxx@gmail.com>
2023-05-24 09:38:38 +0300

    edit logo in title

--- alkenazksa/src/componets/Header.js
+++ alkenazksa/src/componets/Header.js
@@ -4,21 +4,23 @@ import Navbar from "../pages/Navbar";
 export default function Header() {
     return (
         <>
-        <body>
+      
     
            
-           <div>
-           <Link to="/"></Link>
-           </div> 
-           <div>
-            <Link to={Navbar}></Link>
-           </div>
-          
+     
+             
+                    
+                        <Link to="/"></Link>
+                   
+                   
+                    
+                       
+                    
 
            
            
 
-        </body>
+     
          
          
                    
--- alkenazksa/src/pages/Home.js
+++ alkenazksa/src/pages/Home.js
@@ -10,15 +10,25 @@ export default function Home(){
 <nav class="navbar">
 <div class="nav">
     <img src={AlkenazLogo} class="brand-logo" alt="alkenaz"/>
+ 
     <div class="nav-items">
+   
         <div class="search">
+           
             <input type="text" class="search-box" placeholder="search brand, product"/>
             <button class="search-btn">search</button>
         </div>
         <a href="#"><img src={userLogo} alt="user"/></a>
         <a href="#"><img src={cartLogo} alt="cart"/></a>
+
     </div>
+    
 </div>
+
+<ul class="links-container">
+    <li class="link-item"><a href="#" class="link">home</a></li>
+    <li class="link-item"><a href="#" class="link">accessories</a></li>
+</ul>
 </nav>
  )
 }
